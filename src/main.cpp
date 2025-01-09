e9e1adb406ec7e1b5757a26036df2b30b4f541b0
Oliver Olsen <oliver@skou-olsen.dk>
2025-01-09 09:57:16 +0100

    Added reading for voltage

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 //Oliver Olsen s205443
 //Nils Wulff   s223968
 
+// 9a. The LM 35 DZ Sensor has a conversion formula: tempInCelcius = /(v_out-0.5)*100. The v_out = (analog_read)*((voltage_in)/1024)
+// The -0.5 is due to the sensor's ability to read negative temps. Therefor an offset is needed.
+
+// 9b
 
 #include <Arduino.h>
 #define TEMPPIN A0
@@ -12,11 +16,14 @@ float volt = 0;
 float tempMeasured = 0;
 
 
+
+
 int voltConvert(int analogInput);
 
 void setup() {
   Serial.begin(9600);
   pinMode(TEMPPIN, INPUT);
+  
 }
 
 void loop() {
@@ -24,13 +31,19 @@ void loop() {
 
   // Store the new voltage
   volt = voltConvert(analogInput);
+  Serial.print("Voltage measured/calculated: ");
+  Serial.println(volt);
 
   // Temp calculated with formula: temperature_c = voltage * 100
   tempMeasured = volt * 100;
+  Serial.print("Temperature measured in deg c: ");
   Serial.println(tempMeasured);
 
   // Delay added to reduce number print-outs
-  delay(500);
+
+  
+  
+  delay(1000);
 
 }
 
